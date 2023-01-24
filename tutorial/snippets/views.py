34d0a4d71a0679c11b3c7fbde612743a8f2c0824
Hugo Ledertheil Ruffo <hugolr@domain.rj.gov.br>
2023-01-24 10:08:52 -0300

    Usando Apiview baseada em classe

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,63 +1,73 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
-@api_view(['GET', 'POST'])
-def snippet_list(request, format = None):
-    """SNIPPET_LIST
+class SnippetList(APIView):
+    """SnippetList
 
     Args:
-        request : request
-    
+        APIView (view): Apiview
+        
     Def:
-        lista todos os snippets ou cria um novo 
+        Lista todos os snippets ou cria um novo.
+        
     """
-    if request.method == "GET":
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
+    def get(self, request, format=None):
+        snippet = Snippet.objects.all()
+        serializer = SnippetSerializer(snippet, many=True)
         return Response(serializer.data)
     
-    elif request.method == "POST":
-        serializer = SnippetSerializer(data = request.data)
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format = None):
-    """snippet_detail
+class SnippetDetail(APIView):
+    """SnippetDetail
 
     Args:
-        request (_type_): request
-        pk (_type_): integer
+        APIView (decorator): Apiview
         
     Def:
-        Retornar ou trabalhar sobre um snippet identificado pela pk
+        Recupera, atualiza ou deleta um snippet identificado pela pk    
     """
-    
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'GET':
-        serializer = SnippetSerializer(snippet) # note que many = False é default não precisa ser declarado
+    def get_object(self, pk):
+        """get_object
+
+        Args:
+            pk (integer): primarykey
+
+        Raises:
+            Http404: Se o objeto com a Id não existir -> http 404 -> Not found.
+            
+        Def:
+            Função consulta se o objeto existe com a pk informada.    
+        """
+        try:
+            snippet = Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+        
+    def get(self,request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
-    
-    elif request.method == 'PUT':
+        
+    def put(self, request, pk, format=None):
+        snippet= self.get_object(pk)
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
-    
-    elif request.method == 'DELETE':
-        serializer.delete()
+            return Response (serializer.data)
+        
+    def delete(self,request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-        
-        
